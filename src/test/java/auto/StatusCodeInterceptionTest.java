a5fc7d7abdd7982476b8c3d90bf5776f9697692c
igorqa <igorqa@nous.dev>
2026-03-26 20:02:41 +0500

    set headless = true

--- src/test/java/auto/StatusCodeInterceptionTest.java
+++ src/test/java/auto/StatusCodeInterceptionTest.java
@@ -20,7 +20,7 @@ public class StatusCodeInterceptionTest {
     @BeforeEach
     public void setup(){
         playwright = Playwright.create();
-        browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
+        browser = playwright.chromium().launch();
         context = browser.newContext();
         page = context.newPage();
     }
