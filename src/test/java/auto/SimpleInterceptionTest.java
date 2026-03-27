3eb6985fca97496110d6a614ab67fb7fdd206829
igorqa <igorqa@nous.dev>
2026-03-27 10:25:37 +0500

    добавление практического задания №2

--- src/test/java/auto/SimpleInterceptionTest.java
+++ src/test/java/auto/SimpleInterceptionTest.java
@@ -16,7 +16,7 @@ public class SimpleInterceptionTest {
     @BeforeAll
     static void setupAll(){
         playwright = Playwright.create();
-        browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
+        browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(true));
     }
 
     @BeforeEach
