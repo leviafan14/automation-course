d1ad768f925fd9714856dc2676e9156ebf094374
igorqa <igorqa@nous.dev>
2026-04-09 21:45:14 +0500

    ПЗ11

--- src/test/java/auto/DynamicLoadingTest.java
+++ src/test/java/auto/DynamicLoadingTest.java
@@ -16,7 +16,7 @@ public class DynamicLoadingTest {
     @Test
     void testDynamicLoading() {
         playwright = Playwright.create();
-        browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
+        browser = playwright.chromium().launch();
         BrowserContext context = browser.newContext();
 
         // Включаем трассировку
