3ee14a102d61342141dcac052abdc23ba8ea1ff1
igorqa <igorqa@nous.dev>
2026-04-21 20:21:11 +0500

    Практическое задание №28

--- src/test/java/auto/MockedApiTest.java
+++ src/test/java/auto/MockedApiTest.java
@@ -24,8 +24,7 @@ public class MockedApiTest {
     @BeforeAll
     static void setUpClass() {
         playwright = Playwright.create();
-        browser = playwright.chromium().launch(
-                new BrowserType.LaunchOptions().setHeadless(false));
+        browser = playwright.chromium().launch();
 
         // Создаем мок ApiService
         apiService = mock(ApiService.class);
